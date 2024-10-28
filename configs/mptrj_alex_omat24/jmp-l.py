a671c02fcd85e84b30f64ee6d1aff483e7192865
Nima Shoghi <nimashoghi@gmail.com>
2024-10-27 21:59:08 -0400

    Update summit configs

--- configs/mptrj_alex_omat24/jmp-l.py
+++ configs/mptrj_alex_omat24/jmp-l.py
@@ -5,86 +5,115 @@ from pathlib import Path
 
 import nshconfig_extra as CE
 import nshtrainer as nt
-import nshutils as nu
 
 import jmp.configs as jc
 
 base_dir = Path("/gpfs/alpine2/proj-shared/mat273/nimashoghi/")
-working_dir = base_dir / "experiment-data/"
+cwd = base_dir / "experiment-data/"
 ckpt_path = base_dir / "checkpoints/jmp-l.pt"
+env = {
+    "HF_HOME": "/gpfs/alpine2/proj-shared/mat273/nimashoghi/hf",
+}
 
-config = jc.Config.draft()
-
-config.name = "jmp-l"
-config.project = "mptrj-alex-omat24"
-
-config.pretrained_ckpt = CE.CachedPath(uri=ckpt_path)
-config.graph_computer = jc.GraphComputerConfig(
+model_hparams = jc.Config.draft()
+model_hparams.pretrained_ckpt = CE.CachedPath(uri=ckpt_path)
+model_hparams.graph_computer = jc.GraphComputerConfig(
     cutoffs=jc.CutoffsConfig.from_constant(8.0),
     max_neighbors=jc.MaxNeighborsConfig(main=20, aeaint=20, aint=1000, qint=8),
     pbc=True,
     per_graph_radius_graph=True,
 )
-config.ignore_graph_generation_errors = True
+model_hparams.ignore_graph_generation_errors = True
 
 # Optimization and learning rate scheduling
-config.optimization = jc.OptimizationConfig.draft()
-config.optimization.optimizer = nt.configs.AdamWConfig(lr=8.0e-5, weight_decay=0.001)
-config.optimization.separate_lr_multiplier = jc.SeparateLRMultiplierConfig(
-    backbone_multiplier=0.25, rest_multiplier=1.0
+model_hparams.optimization = jc.OptimizationConfig.draft()
+model_hparams.optimization.optimizer = nt.configs.AdamWConfig(
+    lr=2.0e-4, weight_decay=0.001
 )
-config.optimization.lr_scheduler = nt.configs.LinearWarmupCosineDecayLRSchedulerConfig(
-    warmup_duration=nt.configs.StepsConfig(value=5000),
-    warmup_start_lr_factor=0.001,
-    max_duration=nt.configs.StepsConfig(value=1_000_000),
-    min_lr_factor=0.1,
+model_hparams.optimization.separate_lr_multiplier = jc.SeparateLRMultiplierConfig(
+    backbone_multiplier=0.2, rest_multiplier=1.0
+)
+model_hparams.optimization.lr_scheduler = (
+    nt.configs.LinearWarmupCosineDecayLRSchedulerConfig(
+        warmup_duration=nt.configs.StepsConfig(value=2500),
+        warmup_start_lr_factor=0.001,
+        max_duration=nt.configs.EpochsConfig(value=4.0),
+        min_lr_factor=0.1,
+    )
+)
+
+# Linref
+model_hparams.energy_referencer = jc.PerAtomReferencerConfig.linear_reference(
+    "mptrj-salex"
 )
 
 # Heads
-config.targets = jc.TargetsConfig.draft()
-config.targets.energy = jc.EnergyTargetConfig(max_atomic_number=120)
-config.targets.force = jc.ForceTargetConfig()
-config.targets.stress = jc.StressTargetConfig(num_layers=5)
-config.targets.energy_loss_coefficient = 20.0
-config.targets.force_loss_coefficient = 20.0
-config.targets.stress_loss_coefficient = 1.0
-config.primary_metric = nt.configs.MetricConfig(name="energy_mae", mode="min")
+model_hparams.targets = jc.TargetsConfig.draft()
+model_hparams.targets.energy = jc.EnergyTargetConfig(max_atomic_number=120)
+model_hparams.targets.force = jc.ForceTargetConfig()
+model_hparams.targets.stress = jc.StressTargetConfig(num_layers=2)
+model_hparams.targets.energy_loss_coefficient = 100.0
+model_hparams.targets.force_loss_coefficient = 10.0
+model_hparams.targets.stress_loss_coefficient = 1.0
+model_hparams = model_hparams.finalize()
 
 # General trainer settings
-config.trainer.val_check_interval = 0.25
-config.trainer.precision = "16-mixed-auto"
-config.trainer.set_float32_matmul_precision = "medium"
-config.trainer.optimizer.log_grad_norm = True
-config.trainer.optimizer.gradient_clipping = nt.configs.GradientClippingConfig(
+trainer_hparams = nt.TrainerConfig.draft()
+trainer_hparams.name.append("jmp-l")
+trainer_hparams.project = "mptrj-alex-omat24"
+
+trainer_hparams.primary_metric = nt.configs.MetricConfig(name="energy_mae", mode="min")
+trainer_hparams.val_check_interval = 0.25
+trainer_hparams.precision = "16-mixed-auto"
+trainer_hparams.set_float32_matmul_precision = "medium"
+trainer_hparams.optimizer.log_grad_norm = True
+trainer_hparams.optimizer.gradient_clipping = nt.configs.GradientClippingConfig(
     value=100.0, algorithm="norm"
 )
-config.trainer.hf_hub.enable_()
+trainer_hparams.hf_hub.enable_()
 
-config.with_project_root_(working_dir)
-config = config.finalize()
-nu.display(config)
+trainer_hparams = trainer_hparams.with_project_root(cwd)
+trainer_hparams = trainer_hparams.finalize()
 
-data_config = jc.MPTrjAlexOMAT24DataModuleConfig.draft()
-data_config.batch_size = 32
-data_config.num_workers = 8
-data_config.subsample_val = 5_000
-data_config.with_linear_reference_("mptrj-salex")
-data_config = data_config.finalize()
-nu.display(data_config)
+# Data
+data_hparams = jc.MPTrjAlexOMAT24DataModuleConfig.draft()
+data_hparams.batch_size = 32
+data_hparams.num_workers = 8
+data_hparams.subsample_val = 5_000
+data_hparams.salex.local_path = Path("/storage/nima/salex-ocp/hf/")
+data_hparams.omat24.local_path = Path("/storage/nima/omat24/hf/")
+data_hparams.with_linear_reference_("mptrj-salex")
+data_hparams = data_hparams.finalize()
 
-runs = [(config, data_config)]
+runs = [(model_hparams, trainer_hparams, data_hparams)]
 
 # %%
 from jmp.lightning_datamodule import MPTrjAlexOMAT24DataModule
 from jmp.lightning_module import Module
 
 
-def run(config: jc.Config, data_config: jc.MPTrjAlexOMAT24DataModuleConfig):
-    module = Module(config)
-    datamodule = MPTrjAlexOMAT24DataModule(data_config)
-    trainer = nt.Trainer(config)
+def run(
+    model_hparams: jc.Config,
+    trainer_hparams: nt.TrainerConfig,
+    data_hparams: jc.MPTrjAlexOMAT24DataModuleConfig,
+):
+    module = Module(model_hparams)
+    datamodule = MPTrjAlexOMAT24DataModule(data_hparams)
+
+    trainer = nt.Trainer(trainer_hparams)
     trainer.fit(module, datamodule)
 
+
+# %%
+import nshrunner as nr
+
+runs_fast_dev_run = [
+    (model_hparams, trainer_hparams.with_fast_dev_run(8), data_hparams)
+    for model_hparams, trainer_hparams, data_hparams in runs
+]
+
+runner = nr.Runner(run, nr.RunnerConfig(working_dir=cwd, env=env))
+runner.local(runs)
 
 # %%
 import datetime
@@ -93,17 +122,18 @@ import nshrunner as nr
 
 # Summit has no access to the internet, so we need to disable W&B and Hugging Face Hub integrations.
 runs_summit = []
-for config, data_config in runs:
+for config, trainer_config, data_config in runs:
     config = config.model_copy(deep=True)
+    trainer_config = trainer_config.model_copy(deep=True)
     data_config = data_config.model_copy(deep=True)
 
-    config.trainer.hf_hub.disable_()
-    if config.trainer.logging.wandb is not None:
-        config.trainer.logging.wandb.offline_()
+    trainer_config.hf_hub.disable_()
+    if trainer_config.logging.wandb is not None:
+        trainer_config.logging.wandb.offline_()
 
-    runs_summit.append((config, data_config))
+    runs_summit.append((config, trainer_config, data_config))
 
-runner = nr.Runner(run, nr.RunnerConfig(working_dir=working_dir))
+runner = nr.Runner(run, nr.RunnerConfig(working_dir=cwd))
 _ = runner.submit_lsf(
     runs_summit,
     {
